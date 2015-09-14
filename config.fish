b2da9cd921183c6c6dd9399ff189ceeb44233a55
Matouš Kučera <matous.kucera@gmail.com>
2015-09-14 10:37:04 +0200

    docker

--- config.fish
+++ config.fish
@@ -4,6 +4,6 @@ set PATH $PATH (find ~/.gvm/*/current/bin -maxdepth 0)
 set PATH $PATH /usr/local/mysql/bin/
 
 # Docker
-set -x DOCKER_CERT_PATH /Users/kuceram/.boot2docker/certs/boot2docker-vm
+set -x DOCKER_CERT_PATH /Users/matous/.docker/machine/machines/default
 set -x DOCKER_TLS_VERIFY 1
-set -x DOCKER_HOST tcp://192.168.59.103:2376
+set -x DOCKER_MACHINE_NAME default
---
+++ functions/docker-start.fish
@@ -0,0 +1,3 @@
+function docker-start
+  /Applications/Docker/Docker\ Quickstart\ Terminal.app/Contents/Resources/Scripts/start.sh
+end
