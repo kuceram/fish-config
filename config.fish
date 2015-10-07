a80f5a9982e18752a79b28a2f89e9ad0a9fecea6
Matouš Kučera <matous.kucera@gmail.com>
2015-10-07 16:11:45 +0200

    move from gvm to sdkman

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 set -g -x fish_greeting ''
 
-set PATH $PATH (find ~/.gvm/*/current/bin -maxdepth 0)
+set PATH $PATH (find ~/.sdkman/*/current/bin -maxdepth 0)
 set PATH $PATH /usr/local/mysql/bin/
 
 # Docker
