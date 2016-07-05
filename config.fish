aa07116d21edfea00f933d57b3e9bc44d454d8f5
Matouš Kučera <matous.kucera@gmail.com>
2016-07-05 12:48:06 +0200

    remove old docker installation

--- config.fish
+++ config.fish
@@ -3,10 +3,5 @@ set -g -x fish_greeting ''
 set PATH $PATH (find ~/.sdkman/*/current/bin -maxdepth 0)
 set PATH $PATH /usr/local/mysql/bin/
 
-# Docker
-set -x DOCKER_CERT_PATH /Users/matous/.docker/machine/machines/default
-set -x DOCKER_TLS_VERIFY 1
-set -x DOCKER_MACHINE_NAME default
-
 # Java Home
 set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_77.jdk/Contents/Home"
