e0afc6b54fae03d344e1f2942b4772af1bcea7b2
Matouš Kučera <kuceram@Matouss-MBP.zyxel.com>
2017-03-12 15:18:56 +0100

    do not overide jenv java home

--- config.fish
+++ config.fish
@@ -2,6 +2,3 @@ set -g -x fish_greeting ''
 
 set PATH $PATH (find ~/.sdkman/*/current/bin -maxdepth 0)
 set PATH $PATH /usr/local/mysql/bin/
-
-# Java Home
-set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_77.jdk/Contents/Home"
