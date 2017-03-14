3d209e3383cd621981e13e91b9a3fc4d747b7a4f
Matouš Kučera <kuceram@Matouss-MBP.slezska.locusworkspace.cz>
2017-03-14 16:13:37 +0100

    add mysql to path

--- config.fish
+++ config.fish
@@ -2,3 +2,4 @@ set -g -x fish_greeting ''
 
 set PATH $PATH (find ~/.sdkman/*/current/bin -maxdepth 0)
 set PATH $PATH /usr/local/mysql/bin/
+set -g fish_user_paths "/usr/local/opt/mysql@5.6/bin" $fish_user_paths
