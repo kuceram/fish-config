fa71b9c375e217abdbca232c7e3b92d9cb6aeb75
Matouš Kučera <matous.kucera@gmail.com>
2017-11-24 16:30:35 +0100

    feat: add new sdkman

--- config.fish
+++ config.fish
@@ -1,5 +1,5 @@
 set -g -x fish_greeting ''
 
-set PATH $PATH (find ~/.sdkman/*/current/bin -maxdepth 0)
 set PATH $PATH /usr/local/mysql/bin/
+set PATH $PATH /usr/local/sbin
 set -g fish_user_paths "/usr/local/opt/mysql@5.6/bin" $fish_user_paths
